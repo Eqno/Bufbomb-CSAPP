3c9aa5b9d9989601d9ebefd35fd4364ae9e82929
Eqnoxx <Eqnoxx@163.com>
2021-04-27 19:09:32 +0800

    finish

--- fake.c
+++ fake.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void fake()
 {
-	int 
+	int logined = 0;
+	char cmd[10] = "pause", username[10], password[10];
+	scanf("%s%s", username, password);
+	if (strcmp(username, "admin")==0&&strcmp(password, "password")==0)
+	{
+		logined = 1;
+	}
+	if (logined)
+	{
+		puts("logined succeed");
+		system(cmd);
+	}
+	return ;
 }
 
 int main()
 {
-
+	fake();
 	return 0;
 }
